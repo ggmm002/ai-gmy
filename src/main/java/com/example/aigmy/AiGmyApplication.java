0586dda92f1777d29c5ce098212ece7cb422c1e9
gmy <guomaoyangeheh@aliyun.com>
2025-11-20 21:32:50 +0800

    first commit

---
+++ src/main/java/com/example/aigmy/AiGmyApplication.java
@@ -0,0 +1,13 @@
+package com.example.aigmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiGmyApplication.class, args);
+	}
+
+}
